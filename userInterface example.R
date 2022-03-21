5af40f8902fe597fd077f10f5e73eee460fdb428
Sebastiaan van de Velde <42937876+sevdevel@users.noreply.github.com>
2022-03-21 16:55:19 +0100

    Add files via upload

--- userInterface example.R
+++ userInterface example.R
@@ -1,6 +1,6 @@
 ################################################################################
-# Author: Sebastiaan van de Velde (sevdevel@vub.ac.be)
-#         Laurine Burdorf         (Laurine.Burdorf@nioz.nl)
+# Author: Sebastiaan van de Velde (sebastiaan.van.de.velde@ulb.be)
+#         Laurine Burdorf         
 #
 # Example how to use the user interface function of fitprofile to analyse an O2 depth profile. 
 #
@@ -9,31 +9,30 @@
 source("user interface function.R")
 source("FLIPPER_plotfunction.R")
 
+#===============================================================================
+# Test dataset of classical porewater data
+#===============================================================================
 
+load("datasets to test/test.data.1.Rdata")
 
-load("datasets to test/test.data.5.Rdata")
-
-# as.data.frame(summ$input) 
-
-test <- FLIPPER.func(input=as.data.frame(summ$input),species=summ$species,E.cte=c(-0.1,0,0.03),
-             env.parms=summ$env.parms[1:5],discrete.parms=list(LBC="conc.down",C.down=summ$input$C[length(summ$input$C)]),
-             continuous.parms=list(optimal.window.size="interactive"),
-             method="all")
-
-x11(height=20, width=60)
-plot.FLIPPER(test)
-
-
-test <- FLIPPER.func(input=as.data.frame(summ$input),species=summ$species,#E.cte=c(-0.1,0,0.03),
-              env.parms=summ$env.parms[1:5],discrete.parms=list(LBC="conc.down",C.down=summ$input$C[length(summ$input$C)]),
-              continuous.parms=list(optimal.window.size="interactive"),
-              method="gradient")
+test <- FLIPPER.func(input=as.data.frame(test.data$input),species=test.data$species,E.cte=c(-0.1,0,0.03),
+                     env.parms=test.data$env.parms,discrete.parms=list(LBC="conc.down",C.down=test.data$input$C[nrow(test.data$input)]),
+                     continuous.parms=list(optimal.window.size="interactive"),
+                     method="all")
 
 x11(height=20, width=20)
 plot.FLIPPER(test)
 
-test <- FLIPPER.func(input=as.data.frame(summ$input),species=summ$species,#E.cte=c(-0.1,0,0.03),
-                     env.parms=summ$env.parms[1:5],discrete.parms=list(LBC="conc.down",C.down=summ$input$C[length(summ$input$C)]),
+test <- FLIPPER.func(input=as.data.frame(test.data$input),species=test.data$species,E.cte=c(-0.1,0,0.03),
+                     env.parms=test.data$env.parms,discrete.parms=list(LBC="conc.down",C.down=test.data$input$C[nrow(test.data$input)]),
+                     continuous.parms=list(optimal.window.size="interactive"),
+                     method="gradient")
+
+x11(height=20, width=20)
+plot.FLIPPER(test)
+
+test <- FLIPPER.func(input=as.data.frame(test.data$input),species=test.data$species,E.cte=c(-0.1,0,0.03),
+                     env.parms=test.data$env.parms,discrete.parms=list(LBC="conc.down",C.down=test.data$input$C[nrow(test.data$input)]),
                      continuous.parms=list(optimal.window.size="interactive"),
                      method="continuous")
 
@@ -41,11 +40,31 @@ x11(height=20, width=20)
 plot.FLIPPER(test)
 
 
-
-test <- FLIPPER.func(input=as.data.frame(summ$input),species=summ$species,#E.cte=c(-0.1,0,0.03),
-                     env.parms=summ$env.parms[1:5],discrete.parms=list(LBC="conc.down",C.down=summ$input$C[length(summ$input$C)]),
+test <- FLIPPER.func(input=as.data.frame(test.data$input),species=test.data$species,E.cte=c(-0.1,0,0.03),
+                     env.parms=test.data$env.parms,discrete.parms=list(LBC="conc.down",C.down=test.data$input$C[nrow(test.data$input)]),
                      continuous.parms=list(optimal.window.size="interactive"),
                      method="discrete")
 
 x11(height=20, width=20)
 plot.FLIPPER(test)
+
+#===============================================================================
+# Test dataset of microsensor O2 profiling
+#===============================================================================
+
+load("datasets to test/O2_testprofiles.RData")
+
+test <- FLIPPER.func(input=as.data.frame(profile.A[-(1:26),]),species=c("O2"),
+             discrete.parms=list(LBC= "no.flux"),
+             continuous.parms=list(optimal.window.size="interactive"),
+             method="all")
+
+test <- FLIPPER.func(input=as.data.frame(profile.A),species=c("O2"),
+                     discrete.parms=list(LBC= "no.flux"),
+                     continuous.parms=list(optimal.window.size="interactive"),
+                     method="all")
+
+x11(height=20, width=60)
+plot.FLIPPER(test)
+
+
